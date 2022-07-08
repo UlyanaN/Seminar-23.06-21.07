dc52fdc7e3e1ae370c7c7435e727106c2e481f7f
Ulya <Gerony690@yandex.ru>
2022-07-08 17:39:58 +0300

    Task_00

--- Task_00/Program.cs
+++ Task_00/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Квадрат числа
+// Console.WriteLine("Hello, World!");
+Console.Write ("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.Write ($"Квадрат числа {num} = {square} ");
